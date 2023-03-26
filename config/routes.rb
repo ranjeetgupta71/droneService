36a010aa21cac7100d0513259a4b6b40c20929c2
ranjeetgupta71 <ranjeet.github@gmail.com>
2023-03-26 21:13:18 +0530

    adds bookins and customers resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/bookings', to: 'bookings#index'
+  get '/bookings/new', to: 'bookings#new'
+  get '/bookings/:id', to: 'bookings#show'
+  get '/bookings/:id/edit', to: 'bookings#edit'
+
+  get '/customers', to: 'customers#index'
+  get '/customers/new', to: 'customers#new'
+  get '/customers/:id', to: 'customers#show'
+  get '/customers/:id/edit', to: 'customers#edit'
+
+  # get '/patients/:id', to: 'patients#show'
+
 end
