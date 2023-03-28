68a04e96b0d5a76dc6ebdbe3fd74536d3185f70b
ranjeetgupta71 <ranjeet.github@gmail.com>
2023-03-28 22:33:46 +0530

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,28 +2,28 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "bookings#index"
+
+  # bookings related routes
   get '/bookings', to: 'bookings#index'
   get '/bookings/new', to: 'bookings#new'
   get '/bookings/:id', to: 'bookings#show'
   get '/bookings/:id/edit', to: 'bookings#edit'
-
-  get '/customers', to: 'customers#index'
-  get '/customers/new', to: 'customers#new'
-  get '/customers/:id', to: 'customers#show'
-  get '/customers/:id/edit', to: 'customers#edit'
-
-  # posting, updating and deleting bookings
   post '/bookings', to: 'bookings#create'
   put '/bookings/:id', to: 'bookings#update'
   delete '/bookings/:id', to: 'bookings#delete'
 
-  # posting, updating and deleting customers
+  # customers related routes
+  get '/customers', to: 'customers#index'
+  get '/customers/new', to: 'customers#new'
+  get '/customers/:id', to: 'customers#show'
+  get '/customers/:id/edit', to: 'customers#edit'
   post '/customers', to: 'customers#create'
   put '/customers/:id', to: 'customers#update'
   delete '/customers/:id', to: 'customers#delete'
 
   get '/locations', to: 'locations#index'
-  # get '/patients/:id', to: 'patients#show'
+  get '/durations', to: 'durations#index'
 
+  # get '/patients/:id', to: 'patients#show'
 end
